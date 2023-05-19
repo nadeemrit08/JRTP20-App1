69e3136b2773938f5bcf850c39275976f8185843
Nadeem Ahmad <nadeemrit08.com>
2023-05-20 02:43:45 +0700

    first commit

---
+++ src/main/java/in/nadeem/security/PasswordService.java
@@ -0,0 +1,27 @@
+package in.nadeem.security;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class PasswordService {
+
+	public static String encode(String txt) {
+		Encoder encoder = Base64.getEncoder();
+		return encoder.encodeToString(txt.getBytes());
+	}
+
+	public static String decode(String encodeTxt) {
+		Decoder decoder = Base64.getDecoder();
+		byte[] decode = decoder.decode(encodeTxt);
+		return new String(decode);
+	}
+
+	public static void main(String[] args) {
+		String encode = PasswordService.encode("Nadeem");
+		System.out.println(encode);
+
+		String decode = PasswordService.decode(encode);
+		System.out.println(decode);
+	}
+}
